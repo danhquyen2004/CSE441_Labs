65cda2676401fb4f7ffceaf82fffa724ada6d0fd
danhquyen2004 <quyendanh04@gmail.com>
2025-05-16 22:20:54 +0700

    update lab 4, 5

---
+++ Lap11/Lap11_1/app/src/main/java/com/example/intentfilter/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.intentfilter;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnopen;
+    EditText edtlink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtlink = findViewById(R.id.edtLink);
+        btnopen = findViewById(R.id.btnOpen);
+
+        btnopen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("https://" + edtlink.getText().toString()));
+                startActivity(intent);
+            }
+        });
+    }
+}
